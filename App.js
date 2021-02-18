988047afdd09eea57839098bebca047d051bc0c2
Majd-07 <m.shoghri07@gmail.com>
2021-02-18 23:52:56 +0300

    add FlatList and fix CarList importing in App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
-import { CarsList } from './components/CarsList'
+import CarsList from './components/CarsList'
 export default function App() {
 	return (
 		<View style={styles.container}>
--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { View, Text, ImageBackground } from 'react-native'
 import StyledButton from '../StyledButton'
-
 import styles from './styles'
 
 const CarItem = (props) => {
-	const { name, tagline, image } = props.car
+	const { name, tagline, taglineCTA, image } = props.car
 	console.log(props.car)
 	return (
 		<View style={styles.carContainer}>
 			<ImageBackground source={image} style={styles.image} />
+
 			<View style={styles.titles}>
 				<Text style={styles.title}>{name}</Text>
-				<Text style={styles.subtitles}>
+				<Text style={styles.subtitle}>
 					{tagline} <Text style={styles.subtitleCTA}>{taglineCTA}</Text>
 				</Text>
 			</View>
